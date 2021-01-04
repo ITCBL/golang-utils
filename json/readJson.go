643359b3848fed09d42d59887dd1910cbbf75dde
ChenBolong <chenbailong@dm-ai.cn>
2021-01-04 18:06:09 +0800

    readJson

---
+++ json/readJson.go
@@ -0,0 +1,9 @@
+/**
+ * @Author: DM
+ * @Description:
+ * @File:  readJson
+ * @Version: 1.0.0
+ * @Date: 2021/1/4 17:31
+ */
+
+package json
---
+++ json/readJson_test.go
@@ -0,0 +1,9 @@
+/**
+ * @Author: DM
+ * @Description:
+ * @File:  readJson_test
+ * @Version: 1.0.0
+ * @Date: 2021/1/4 17:39
+ */
+
+package json
